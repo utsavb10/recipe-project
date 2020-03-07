3fd89f2c5dc7fade7672a51e355ba3e88b3fbfdc
Utsav <utsav@test.com>
2020-03-07 19:03:05 +0530

    *Created 2 Domain entities, Recipe and Notes *Gave 1-1 relation to Recipe and Notes. Recipe is the owner. *Identified @Lob fields.

---
+++ src/main/java/chela/springframework/recipeproject/controller/IndexController.java
@@ -0,0 +1,13 @@
+package chela.springframework.recipeproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping({"", "/"})
+	public String getIndexPage(){
+		return "index";
+	}
+}
