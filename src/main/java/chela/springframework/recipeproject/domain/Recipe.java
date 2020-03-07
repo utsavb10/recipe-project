5c1b1cde8f692298d6ad4d00e6e5b6e76a37cb7b
Utsav <utsav@test.com>
2020-03-07 19:18:56 +0530

    *Created Ingredient Domain *OneToMany btw Recipe and Ingredient *ManyToOne btw Ingredient and Recipe

--- src/main/java/chela/springframework/recipeproject/domain/Recipe.java
+++ src/main/java/chela/springframework/recipeproject/domain/Recipe.java
@@ -1,6 +1,7 @@
 package chela.springframework.recipeproject.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -22,6 +23,20 @@ public class Recipe {
 
 	@OneToOne(cascade = CascadeType.ALL)
 	private Notes notes;
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+	private Set<Ingredient> ingredients;
+
+//	-------------------------------------------------------------------------------
+
+
+	public Set<Ingredient> getIngredients() {
+		return ingredients;
+	}
+
+	public void setIngredients(Set<Ingredient> ingredients) {
+		this.ingredients = ingredients;
+	}
 
 	public Long getId() {
 		return id;
