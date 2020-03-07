7db48d208c5c8bc22386830cbb5fd93835a51f6b
Utsav <utsav@test.com>
2020-03-07 21:03:52 +0530

    *created queryMethod using JPA

--- src/main/java/chela/springframework/recipeproject/controller/IndexController.java
+++ src/main/java/chela/springframework/recipeproject/controller/IndexController.java
@@ -1,13 +1,33 @@
 package chela.springframework.recipeproject.controller;
 
+import chela.springframework.recipeproject.domain.Category;
+import chela.springframework.recipeproject.domain.UnitOfMeasure;
+import chela.springframework.recipeproject.repository.CategoryRepository;
+import chela.springframework.recipeproject.repository.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+	private CategoryRepository categoryRepository;
+	private UnitOfMeasureRepository unitOfMeasureRepository;
+
+	public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+		this.categoryRepository = categoryRepository;
+		this.unitOfMeasureRepository = unitOfMeasureRepository;
+	}
+
 	@RequestMapping({"", "/"})
 	public String getIndexPage(){
+		Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+		Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+		System.out.println("cat id is= "+ categoryOptional.get().getId());
+		System.out.println("uom id is= "+ unitOfMeasureOptional.get().getId());
+
 		return "index";
 	}
 }
--- src/main/java/chela/springframework/recipeproject/domain/Category.java
+++ src/main/java/chela/springframework/recipeproject/domain/Category.java
@@ -9,7 +9,7 @@ public class Category {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String categoryName;
+	private String description;
 
 	@ManyToMany(mappedBy = "categories")
 	private Set<Recipe> recipeSet;
@@ -22,12 +22,12 @@ public class Category {
 		this.id = id;
 	}
 
-	public String getCategoryName() {
-		return categoryName;
+	public String getDescription() {
+		return description;
 	}
 
-	public void setCategoryName(String categoryName) {
-		this.categoryName = categoryName;
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
 	public Set<Recipe> getRecipeSet() {
