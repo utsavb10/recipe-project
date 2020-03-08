7527f88aef4739c95a78ef7ba4759dfa1174d6b6
Utsav <utsav@test.com>
2020-03-08 15:01:32 +0530

    *refactored remaianing enitities with @Data

--- src/main/java/chela/springframework/recipeproject/domain/Ingredient.java
+++ src/main/java/chela/springframework/recipeproject/domain/Ingredient.java
@@ -1,8 +1,11 @@
 package chela.springframework.recipeproject.domain;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 
+@Data
 @Entity
 public class Ingredient {
 
@@ -25,43 +28,4 @@ public class Ingredient {
 		this.recipe = recipe;
 	}
 
-	public UnitOfMeasure getUom() {
-		return uom;
-	}
-
-	public void setUom(UnitOfMeasure uom) {
-		this.uom = uom;
-	}
-
-	public Recipe getRecipe() {
-		return recipe;
-	}
-
-	public void setRecipe(Recipe recipe) {
-		this.recipe = recipe;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public BigDecimal getAmount() {
-		return amount;
-	}
-
-	public void setAmount(BigDecimal amount) {
-		this.amount = amount;
-	}
 }
